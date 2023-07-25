05c9e0bbb0aa95b0b9339475eaf4f00f41b05ad6
a3cb <a3cb1903@gmail.com>
2023-07-25 22:54:19 +0200

    feat: spell pickups now respawn

--- Elympics-Game/Assets/Scripts/Items/SpellPickup.cs
+++ Elympics-Game/Assets/Scripts/Items/SpellPickup.cs
@@ -1,16 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Elympics;
+using Unity.VisualScripting;
 
-public class SpellPickup : MonoBehaviour
+public class SpellPickup : ElympicsMonoBehaviour, IUpdatable
 {
     [SerializeField] protected int spellID = -1;
+    [SerializeField] private float respawnTime = 5.0f;
+    public ElympicsFloat timeToSpawn = new ElympicsFloat();
+    private ElympicsBool empty = new ElympicsBool(false);
+    public void Awake()
+    {
+        Color color = (spellID == 0) ? Color.red : Color.yellow;
+        GetComponentInChildren<Renderer>().material.SetColor("_Color",color);
+        empty.ValueChanged += ChangeSpellPickup;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        //TODO: this should be reworked as well as ActionHandler.addSpell, collected spells should be synchronised and controlled by server
         if (other.GetComponent<ActionHandler>() != null)
         {
-            if(other.GetComponent<ActionHandler>().addSpell(spellID))
-            {gameObject.SetActive(false);}
+            if (other.GetComponent<ActionHandler>().addSpell(spellID))
+            {
+                timeToSpawn.Value = respawnTime;
+                empty.Value = true;
+                GetComponent<Collider>().enabled = false;
+                GetComponentInChildren<Renderer>().material.SetColor("_Color",Color.black);
+            }
+        }
+    }
+
+    public void ElympicsUpdate()
+    {
+        if (!empty.Value) return;
+        if (timeToSpawn.Value > 0.0f)
+        {
+            timeToSpawn.Value -= Elympics.TickDuration;
+        }
+        else
+        {
+            empty.Value = false;
+        }
+    }
+
+    private void ChangeSpellPickup(bool oldVal, bool newVal)
+    {
+        if (!newVal)
+        {
+            GetComponent<Collider>().enabled = true;
+            Color color = (spellID == 0) ? Color.red : Color.yellow;
+            GetComponentInChildren<Renderer>().material.SetColor("_Color",color);
         }
     }
 }
