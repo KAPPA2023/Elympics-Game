e3ad366d155f9c2cf868879c9247a882efe85302
a3cb <a3cb1903@gmail.com>
2023-07-25 23:25:04 +0200

    feat: matchmaking info in menu

--- Elympics-Game/Assets/Scripts/MatchmakingManager.cs
+++ Elympics-Game/Assets/Scripts/MatchmakingManager.cs
@@ -2,11 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Elympics;
+using Elympics.Models.Matchmaking;
+using TMPro;
 
 public class MatchmakingManager : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> buttons;
+    [SerializeField] private TextMeshProUGUI matchStatus;
+    
+    private void Start()
+    {
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingStarted += DisplayMatchmakingStarted;
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingMatchFound += _ => DisplayMatchFound();
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingFailed += _ => DisplayMatchmakingError();
+    }
+
+    private void OnDestroy()
+    {
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingStarted -= DisplayMatchmakingStarted;
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingMatchFound -= _ => DisplayMatchFound();
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingFailed -= _ => DisplayMatchmakingError();
+    }
+    
     public void PlayOnline()
     {
         ElympicsLobbyClient.Instance.PlayOnlineInRegion(null, null,null, "Default");
+        
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingStarted += DisplayMatchmakingStarted;
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingMatchFound += _ => DisplayMatchFound();
+        ElympicsLobbyClient.Instance.Matchmaker.MatchmakingFailed += _ => DisplayMatchmakingError();
+        
+    }
+    
+    private void DisplayMatchmakingStarted()
+    {
+        matchStatus.text = "looking for game :)";
+
+        foreach (var button in buttons)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    private void DisplayMatchFound()
+    {
+        matchStatus.text  = "game found POG";
+    }
+    
+    private void DisplayMatchmakingError()
+    {
+        matchStatus.text = "unlucky ... maybe try again?";
+        
+        foreach (var button in buttons)
+        {
+            button.SetActive(true);
+        }
     }
 }
