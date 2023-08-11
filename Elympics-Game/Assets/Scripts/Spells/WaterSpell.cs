987624fad508e2b07b762617e709ce9018f1e7b6
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2023-08-11 20:30:05 +0200

    fix

--- Elympics-Game/Assets/Scripts/Spells/WaterSpell.cs
+++ Elympics-Game/Assets/Scripts/Spells/WaterSpell.cs
@@ -10,7 +10,11 @@ public class WaterSpell : Spell
    protected override void OnTriggerEnter(Collider other)
    {
       base.OnTriggerEnter(other);
-      
+      var playerInfo = other.GetComponent<PlayerData>();
+      if (playerInfo != null)
+      {
+         playerInfo.GetComponent<StatsController>().isBlind.Value = true;
+      }
       
    }
    
