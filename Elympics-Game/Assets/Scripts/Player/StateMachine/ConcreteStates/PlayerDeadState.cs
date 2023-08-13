ba1b6c1e045371149c118f17de8cf2fa61002319
a3cb <a3cb1903@gmail.com>
2023-08-13 16:14:54 +0200

    feat: second player dead on client side

--- Elympics-Game/Assets/Scripts/Player/StateMachine/ConcreteStates/PlayerDeadState.cs
+++ Elympics-Game/Assets/Scripts/Player/StateMachine/ConcreteStates/PlayerDeadState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerDeadState : PlayerState
 {
-    public PlayerDeadState(PlayerController player, PlayerStateMachine stateMachine) : base(player, stateMachine)
+    public PlayerDeadState(PlayerController player, PlayerStateMachine stateMachine, DeathController deathController) : base(player, stateMachine, deathController)
     {
     }
 
@@ -15,7 +15,6 @@ public class PlayerDeadState : PlayerState
         player.GetComponent<Rigidbody>().useGravity = false;
         player.GetComponent<Rigidbody>().velocity = Vector3.zero;
         player.GetComponent<Collider>().enabled = false;
-        Debug.Log("You're dead");
     }
 
     public override void ExitState()
@@ -23,17 +22,11 @@ public class PlayerDeadState : PlayerState
         base.ExitState();
         player.GetComponent<Collider>().enabled = true;
         player.GetComponent<Rigidbody>().useGravity = true;
-        Debug.Log("You're alive");
     }
 
     public override void PlayerElympicsUpdate()
     {
         base.PlayerElympicsUpdate();
-
-        if (!player.isDead())
-        {
-            stateMachine.ChangeState(player.NormalState);
-        }
     }
 
     public override void InputUpdate()
