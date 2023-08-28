b9d5138aa08d31e8a939b97d477707a1154dc74f
PrzeSwiat <101842715+PrzeSwiat@users.noreply.github.com>
2023-08-28 12:44:49 +0200

    Fixed: Tutorial drawing spells, tutorial subtitles

--- Elympics-Game/Assets/Scripts/Arena/TutorialTextTriggers.cs
+++ Elympics-Game/Assets/Scripts/Arena/TutorialTextTriggers.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TutorialTriggerMultiple : MonoBehaviour
+public class TutorialTextTriggers : MonoBehaviour
 {
-    public Text tutorialText;
+    public TMP_Text tutorialText;
     public string triggerMessage; 
 
     private void OnTriggerEnter(Collider other)
