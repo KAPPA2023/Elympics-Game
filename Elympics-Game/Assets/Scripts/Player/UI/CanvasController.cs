d0471d1ed2eb2f1d437738f5a8a86c5ff72f25f3
a3cb <a3cb1903@gmail.com>
2023-09-05 22:51:03 +0200

    feat: disabling input for tutorial

--- Elympics-Game/Assets/Scripts/Player/UI/CanvasController.cs
+++ Elympics-Game/Assets/Scripts/Player/UI/CanvasController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class CanvasController : MonoBehaviour
 {
     public GameObject button;
+    [SerializeField] private GameObject background;
+    [SerializeField] private InputProvider playerInput;
 
     private void Start()
     {
         button.SetActive(false);
+        background.SetActive(false);
     }
 
     private void Update()
@@ -22,7 +25,10 @@ public class CanvasController : MonoBehaviour
 
     private void ToggleButton()
     {
-        button.SetActive(!button.activeSelf); 
+        button.SetActive(!button.activeSelf);
+        background.SetActive(button.activeSelf);
+        playerInput.enabled = !button.activeSelf;
+
     }
 
 }
